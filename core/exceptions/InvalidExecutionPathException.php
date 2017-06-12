c271bd48ba47494131a1f7c6db5732b0461dfa0b
Darren Ethier <darren@roughsmootheng.in>
2017-06-12 08:23:49 -0400

    add new exception [touch: 10236]

---
+++ core/exceptions/InvalidExecutionPathException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace EventEspresso\core\exceptions;
+
+use RuntimeException;
+
+defined('EVENT_ESPRESSO_VERSION') || exit('No direct access allowed.');
+
+class InvalidExecutionPathException extends RuntimeException
+{
+
+}
